c10cc45b6e429b3da313d3c243b04ceaba95d5a3
Kofi Boakye <66253143+boakyezitek@users.noreply.github.com>
2024-01-10 01:22:12 +0000

    Update PropertyController.php

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Property;
-use App\Http\Requests\StorePropertyRequest;
-use App\Http\Requests\UpdatePropertyRequest;
 use App\Http\Resources\PropertyResource;
 use App\Models\Validators\PropertyValidator;
 use Carbon\Carbon;
